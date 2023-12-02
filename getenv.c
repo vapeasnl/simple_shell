78649fc6bdfab5641e3aec213bd37de9866920e1
abdessamadsalim <samimalim1991@gmail.com>
2023-12-02 11:58:40 -0800

    task

---
+++ getenv.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+char *_getenv(char *var);
+{
+    char *tmp, *key, *val, *env;
+    int i;
+
+    for (i = 0; environ[i]; i++)
+    {
+        tmp = _stdrup(environ[i]);
+        key = strtok(tmp, "=");
+        if(_strcmp(key, var) == 0)
+        {
+            val = strtok(NULL, "\n");
+            env = _stdrup(val);
+            free(tmp);
+            return (val);
+        }
+        free(tmp), tmp =NULL;
+    }
+    return (NULL);
+}
